9006f4d90e320b18146ae4487f41b3baff5386b4
Nicholas Bundy <nick.bundy@protonmail.com>
2024-06-05 19:52:18 -0400

    setup script

---
+++ setup_files.sh
@@ -0,0 +1,4 @@
+ln /home/oomi/src/dotfiles/init.vim ~/.config/nvim/init.vim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
