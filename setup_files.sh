6f39c10a8f46815d03f9fae60e5867135a9a08ae
Nicholas <nick.bundy@protonmail.com>
2024-11-12 14:02:23 -0500

    Fix the setup script so it works with any directory structure. Also I think I added the thing to download vim plug here? Not sure. Whatever.

--- setup_files.sh
+++ setup_files.sh
@@ -1,4 +1,5 @@
-ln /home/oomi/src/dotfiles/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim
+ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
